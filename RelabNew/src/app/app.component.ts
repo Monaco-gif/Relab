3f8cf6c8460fbdd28dbf5e17950102603af37ce0
Monaco-gif <monaco.matteo@iisgalvanimi.edu.it>
2021-09-23 09:57:34 +0000

    lab01.1

--- RelabNew/src/app/app.component.ts
+++ RelabNew/src/app/app.component.ts
@@ -11,11 +11,34 @@ export class AppComponent {
   center : any;
   position : any;
   label :string;
+  circleOptions: { fillColor: string; };
+  circleOptions2: { fillColor: string; };
   constructor()
   {
     this.center={lat: 45.506738, lng: 9.190766};
     this.position = this.center;
     this.label = "Sei Qui";
+    this.circleOptions = {fillColor : 'red'}
+    this.circleOptions2 = {fillColor : 'blue'}
   }
 
+  yellow(){
+    this.circleOptions = {fillColor : 'yellow'}
+  }
+  black(){
+    this.circleOptions = {fillColor : 'black'}
+  }
+  green(){
+    this.circleOptions = {fillColor : 'green'}
+  }
+
+  pink(){
+    this.circleOptions2 = {fillColor : 'pink'}
+  }
+  purple(){
+    this.circleOptions2 = {fillColor : 'purple'}
+  }
+  orange(){
+    this.circleOptions2 = {fillColor : 'orange'}
+  }
 }
