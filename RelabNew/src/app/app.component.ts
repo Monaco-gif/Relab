af17182fd89a9c33dda614d70249606c34652c48
Monaco-gif <monaco.matteo@iisgalvanimi.edu.it>
2021-10-01 09:52:11 +0000

    fino alle immagini

--- RelabNew/src/app/app.component.ts
+++ RelabNew/src/app/app.component.ts
@@ -7,19 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'server mappe';
+  markerOptions: google.maps.MarkerOptions;
+  markerOptions2: google.maps.MarkerOptions;
   //Aggiungiamo latitudine e longitudine di un luogo
   center : any;
   position : any;
   label :string;
   circleOptions: { fillColor: string; };
   circleOptions2: { fillColor: string; };
+  position2: { lat: number; lng: number; };
   constructor()
   {
     this.center={lat: 45.506738, lng: 9.190766};
     this.position = this.center;
+    this.position2 = {lat: 45.507338, lng: 9.190766};
     this.label = "Sei Qui";
     this.circleOptions = {fillColor : 'red'}
     this.circleOptions2 = {fillColor : 'blue'}
+
+    let iconData : google.maps.Icon = {
+      url:'./assets/img/cat_acrobat.ico',
+      scaledSize : new google.maps.Size(60,60)
+    }
+    this.markerOptions = {icon:iconData}
+
+    let iconData2 : google.maps.Icon = {
+      url:'./assets/img/acquila.jpg',
+      scaledSize : new google.maps.Size(60,60)
+    }
+    this.markerOptions2 = {icon:iconData2}
   }
 
   yellow(){
@@ -41,4 +57,7 @@ export class AppComponent {
   orange(){
     this.circleOptions2 = {fillColor : 'orange'}
   }
+
+  
 }
+
