eb8c3aacc3bda75292ab3105637f142eb2626187
Monaco-gif <monaco.matteo@iisgalvanimi.edu.it>
2021-10-07 09:54:59 +0000

    rettangolo

--- RelabNew/src/app/app.component.ts
+++ RelabNew/src/app/app.component.ts
@@ -11,62 +11,76 @@ export class AppComponent {
   markerOptions2: google.maps.MarkerOptions;
   markerOptions3: google.maps.MarkerOptions;
   //Aggiungiamo latitudine e longitudine di un luogo
-  center : any;
-  position : any;
-  label :string;
+  center: any;
+  position: any;
+  label: string;
   circleOptions: { fillColor: string; };
   circleOptions2: { fillColor: string; };
   position2: { lat: number; lng: number; };
   position3: { lat: number; lng: number; };
-  constructor()
-  {
-    this.center={lat: 45.506738, lng: 9.190766};
+  vertices: google.maps.LatLngLiteral[];
+  vertices2: google.maps.LatLngLiteral[];
+  constructor() {
+    this.center = { lat: 45.506738, lng: 9.190766 };
     this.position = this.center;
-    this.position2 = {lat: 45.507338, lng: 9.190766};
-    this.position3={lat: 45.506038, lng: 9.190766};
+    this.position2 = { lat: 45.507338, lng: 9.190766 };
+    this.position3 = { lat: 45.506038, lng: 9.190766 };
     this.label = "Sei Qui";
-    this.circleOptions = {fillColor : 'red'}
-    this.circleOptions2 = {fillColor : 'blue'}
+    this.circleOptions = { fillColor: 'red' }
+    this.circleOptions2 = { fillColor: 'blue' }
 
-    let iconData : google.maps.Icon = {
-      url:'./assets/img/cat_acrobat.ico',
-      scaledSize : new google.maps.Size(60,60)
+    let iconData: google.maps.Icon = {
+      url: './assets/img/cat_acrobat.ico',
+      scaledSize: new google.maps.Size(60, 60)
     }
-    this.markerOptions = {icon:iconData}
+    this.markerOptions = { icon: iconData }
 
-    let iconData2 : google.maps.Icon = {
-      url:'./assets/img/acquila.jpg',
-      scaledSize : new google.maps.Size(60,60)
+    let iconData2: google.maps.Icon = {
+      url: './assets/img/acquila.jpg',
+      scaledSize: new google.maps.Size(60, 60)
     }
-    this.markerOptions2 = {icon:iconData2}
+    this.markerOptions2 = { icon: iconData2 }
 
-    let iconData3 : google.maps.Icon = {
-      url:'./assets/img/snake.jfif',
-      scaledSize : new google.maps.Size(60,60)
+    let iconData3: google.maps.Icon = {
+      url: './assets/img/snake.jfif',
+      scaledSize: new google.maps.Size(60, 60)
     }
-    this.markerOptions3 = {icon:iconData3}
+    this.markerOptions3 = { icon: iconData3 }
+
+    this.vertices = [
+      { lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      { lat: this.center.lat, lng: this.center.lng },
+      { lat: this.center.lat - 0.001, lng: this.center.lng - 0.002 }
+    ];
+
+    this.vertices2 = [
+      { lat: 45.507338, lng: 9.190766 },
+      { lat: 45.507338, lng: 9.191966 },
+      { lat: 45.506038, lng: 9.191966 },
+      { lat: 45.506038, lng: 9.190766 }
+    ];
   }
 
-  yellow(){
-    this.circleOptions = {fillColor : 'yellow'}
+  yellow() {
+    this.circleOptions = { fillColor: 'yellow' }
   }
-  black(){
-    this.circleOptions = {fillColor : 'black'}
+  black() {
+    this.circleOptions = { fillColor: 'black' }
   }
-  green(){
-    this.circleOptions = {fillColor : 'green'}
-  }
-
-  pink(){
-    this.circleOptions2 = {fillColor : 'pink'}
-  }
-  purple(){
-    this.circleOptions2 = {fillColor : 'purple'}
-  }
-  orange(){
-    this.circleOptions2 = {fillColor : 'orange'}
+  green() {
+    this.circleOptions = { fillColor: 'green' }
   }
 
-  
+  pink() {
+    this.circleOptions2 = { fillColor: 'pink' }
+  }
+  purple() {
+    this.circleOptions2 = { fillColor: 'purple' }
+  }
+  orange() {
+    this.circleOptions2 = { fillColor: 'orange' }
+  }
+
+
 }
 
